bdf13fe0c4cd9106bd375e8efe1c271ac7e201cb
MiticoPlayer <aperper.4@gmail.com>
2022-06-30 11:33:39 +0200

    feat: Some basic styles

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  var height = $(window).height();
+  height = height - 20;
+  $("#div1").height(height);
+  height = height / 2;
+  $("#div2").height(height);
+  $("#div3").height(height);
+});
