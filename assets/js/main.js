2334bdaba413910ff6246986985a246b81ab3771
MiticoPlayer <aperper.4@gmail.com>
2022-07-04 23:16:14 +0200

    feat: api experiments

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,21 @@ $(document).ready(function () {
   $("#div2").height(height);
   $("#div3").height(height);
 });
+
+window.addEventListener("load", () => {
+  let name = "harry";
+  let type = "movie";
+  const url = `http://www.omdbapi.com/?apikey=ad24807e&t=${name}&type=${type}`;
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.Poster);
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
